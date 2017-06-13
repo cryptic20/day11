03891361647b58925e5a7305fb675b312ce1286d
cryptic20 <sherwin.wyco20@gmail.com>
2017-06-13 12:25:20 -0400

    edited

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 var pg = require('pg');
 
 //connect to database
-pg.connect('postgres://postgres:kabayoka123@localhost:8080/class_db', function(err, client, done){
+pg.connect('postgres://postgres:kabayoka123@localhost:8080/bulletinboard', function(err, client, done){
 
 
-// client.query(`insert into public.hats
-//  (name, material, height, brim)
+// client.query(`insert into public.bulletinboard
+//  (title, body)
 //  values
-//  ('mafia', 'fedora', '3', true)`, function(err, result) {
+//  ('mafia', 'fedora')`, function(err, result) {
 //  //should print 'INSERT: 1'
 //  console.log(`${result.command}: ${result.rowCount}`);
 
@@ -19,7 +19,7 @@ pg.connect('postgres://postgres:kabayoka123@localhost:8080/class_db', function(e
 // });
 
 
-// client.query(`select * from public.hats`, function(err, result){
+// client.query(`select * from public.bulletinboard`, function(err, result){
 // 	console.log(result.rows);
 // 	done();
 
@@ -28,22 +28,22 @@ pg.connect('postgres://postgres:kabayoka123@localhost:8080/class_db', function(e
 
 
 
-client.query(`create TABLE public.bulletinboard
-(id serial primary key,  id	serial primary key
-title	text
-body	text
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;`,function (err, result){
-    	console.log("created");
+// client.query(`create TABLE public.bulletinboard
+// (id	serial primary key,
+// title	text,
+// body	text
+// )
+// WITH (
+//     OIDS = FALSE
+// )
+// TABLESPACE pg_default;`,function (err, result){
+//     	console.log("created");
 
-    	done();
+//     	done();
 
-    	pg.end();
-    });
+//     	pg.end();
+//     });
 
-});
+// });
 
 
