ebeb1cf7429eacf25e92911d3ff2533b85b82864
cryptic20 <sherwin.wyco20@gmail.com>
2017-06-19 14:41:19 -0400

    push to heroku

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app     = express();
+var path    = require("path");
+
+app.listen(process.env.PORT || 3000, function(){
+	console.log('listening to port 3000');
+});
+
+app.use(express.static('files'))
+
+ app.get('/',function(req, res){
+ 	res.sendFile('index.html');
+
+ })
+
+ app.get('/blog',function(req,res){
+ 	res.sendFile(path.join(__dirname, '/files', 'blog.html'));
+
+
+ })
+
+ app.get('/about',function(req,res){
+ 	res.sendFile(path.join(__dirname, '/files', 'about.html'));
+
+
+ })
